a6f0b05c7151d205fc748fe9ae0a11c246eb9566
rchan26 <rchan@turing.ac.uk>
2019-06-10 11:28:36 +0100

    remove old

--- layeredBB/inc/inverse_gauss.hpp
+++
@@ -1,7 +0,0 @@
-#ifndef INVERSE_GAUSS
-#define INVERSE_GAUSS
-
-// forward declaration for simulation of Inverse Gaussian variable
-double inv_gauss_sampler(const double &mu, const double &lambda);
-
-#endif
